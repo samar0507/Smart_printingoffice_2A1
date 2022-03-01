d8427bd16dfc42f998b4e8cb877b1f80febeb723
samar0507 <79337944+samar0507@users.noreply.github.com>
2022-03-01 11:22:14 +0100

    combobox

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "client.h"
-
+    #include <QComboBox>
 #include <QLineEdit>
 #include <QMessageBox>
 #include <QIntValidator>
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->tab_cl->setModel(cl.afficher());
+    QSqlQueryModel *model = new QSqlQueryModel;
+        model->setQuery("SELECT cin FROM client");
+        ui->comboBox->setModel(model);
 }
 
 MainWindow::~MainWindow()
@@ -21,12 +24,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_4_clicked()
 {
-    int cin = ui->lineEdit_idsup->text().toInt();
 
+int cin=ui->comboBox->currentText().toInt();
     bool test=cl.supprimer(cin);
     if(test)
     { ui->tab_cl->setModel(cl.afficher());//refresh
-        ui->lineEdit_idsup->clear();
+        ui->comboBox->clear();
+        QSqlQueryModel *model = new QSqlQueryModel;
+            model->setQuery("SELECT cin FROM client");
+            ui->comboBox->setModel(model);
 
         QMessageBox::information(nullptr, QObject::tr("done"),
                                QObject::tr("suppression effectue.\n"
@@ -53,6 +59,10 @@ void MainWindow::on_pushButton_20_clicked()
        bool test=cl.ajouter();
        if(test)
        { ui->tab_cl->setModel(cl.afficher());//refresh
+           ui->comboBox->clear();
+           QSqlQueryModel *model = new QSqlQueryModel;
+               model->setQuery("SELECT cin FROM client");
+               ui->comboBox->setModel(model);
            ui->lineEdit_cin->clear();
            ui->lineEdit_nom->clear();
            ui->lineEdit_prenom->clear();
@@ -82,6 +92,10 @@ void MainWindow::on_pushButton_19_clicked()
       bool test=cl.modifier(cin);
       if(test)
       { ui->tab_cl->setModel(cl.afficher());//refresh
+          QSqlQueryModel *model = new QSqlQueryModel;
+              model->setQuery("SELECT cin FROM client");
+              ui->comboBox->setModel(model);
+              ui->comboBox->clear();
           ui->lineEdit_cin->clear();
           ui->lineEdit_nom->clear();
           ui->lineEdit_prenom->clear();
@@ -98,3 +112,4 @@ void MainWindow::on_pushButton_19_clicked()
                         QObject::tr("modification non effectué !.\n"
                                     "Click Cancel to exit."), QMessageBox::Cancel);
 }
+
