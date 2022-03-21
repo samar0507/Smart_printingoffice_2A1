e03929a835b236f2c7b827ff02234815b0801e86
GSouissi01 <ghada.souissi@esprit.tn>
2022-03-21 09:59:51 +0100

    test

---
+++ french.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>fenetre</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>Submit</source>
+        <translation>valider</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="39"/>
+        <source>Change Font</source>
+        <translation>changer la police</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Name</source>
+        <translation>nom</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>Surname</source>
+        <translation>prenom</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>Age</source>
+        <translation>age</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <source>Profession</source>
+        <translation>travail</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="32"/>
+        <source>message</source>
+        <translation>message</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="32"/>
+        <source>User %1 %2 is %3 years old and works as %4</source>
+        <translation>utilisateur %1 %2 est %3 et est %4</translation>
+    </message>
+</context>
+</TS>
