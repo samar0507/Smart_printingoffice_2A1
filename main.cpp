f2210a77e6ddf48ed2922b658c6dc351ef6484f7
GSouissi01 <ghada.souissi@esprit.tn>
2022-03-21 09:45:29 +0100

    test
    test

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include<QTranslator>
+#include <QApplication>
+#include <QInputDialog>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTranslator t;
+    QStringList languages;
+    languages << "English" << "French"  ;
+    QString lang= QInputDialog::getItem(NULL,"Select Language",
+                                        "Language", languages);
+    if(lang == "French")
+    {
+        t.load(":/french.qm");
+    }else if(lang != "English")
+    {
+        a.installTranslator(&t);
+    }
+
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
